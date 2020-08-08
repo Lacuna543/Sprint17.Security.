0205d0b748c54a93d58de11555782e15606f9f22
Nazar Panasiuk <panasiuk.n.r@gmail.com>
2020-08-08 21:55:08 +0300

    Add login page controller

---
+++ src/main/java/com/softserve/edu/controller/LoginController.java
@@ -0,0 +1,14 @@
+package com.softserve.edu.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class LoginController {
+
+    @GetMapping("/form-login")
+    public String login() {
+        return "login-page";
+    }
+
+}
